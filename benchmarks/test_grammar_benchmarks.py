0c19d3f7eaa05191d61a8158d176f3772ad69773
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-06 19:40:12 -0400

    fix: grammer

--- benchmarks/test_grammar_benchmarks.py
+++ benchmarks/test_grammar_benchmarks.py
@@ -1,6 +1,6 @@
 import pytest
 
-import eth_abi.grammer
+import eth_abi.grammar
 from pytest_codspeed import (
     BenchmarkFixture,
 )
@@ -14,7 +14,7 @@ import faster_eth_abi.grammar
 @pytest.mark.benchmark(group="GrammarNormalize")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_normalize(benchmark: BenchmarkFixture, type_str):
-    benchmark(eth_abi.grammer.normalize, type_str)
+    benchmark(eth_abi.grammar.normalize, type_str)
 
 
 @pytest.mark.benchmark(group="GrammarNormalize")
@@ -26,7 +26,7 @@ def test_faster_normalize(benchmark: BenchmarkFixture, type_str):
 @pytest.mark.benchmark(group="GrammarParse")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_parse(benchmark: BenchmarkFixture, type_str):
-    benchmark(eth_abi.grammer.parse, type_str)
+    benchmark(eth_abi.grammar.parse, type_str)
 
 
 @pytest.mark.benchmark(group="GrammarParse")
