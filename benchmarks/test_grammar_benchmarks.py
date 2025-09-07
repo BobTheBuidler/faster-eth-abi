5bb51d8278578fe51077aa7eba859738cd4276c3
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-09-07 00:13:41 +0000

    batch x1000

--- benchmarks/test_grammar_benchmarks.py
+++ benchmarks/test_grammar_benchmarks.py
@@ -5,6 +5,9 @@ from pytest_codspeed import (
     BenchmarkFixture,
 )
 
+from benchmarks.batch import (
+    batch,
+)
 from benchmarks.type_strings import (
     type_strings,
 )
@@ -14,22 +17,22 @@ import faster_eth_abi.grammar
 @pytest.mark.benchmark(group="GrammarNormalize")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_normalize(benchmark: BenchmarkFixture, type_str):
-    benchmark(eth_abi.grammar.normalize, type_str)
+    benchmark(batch, 1000, eth_abi.grammar.normalize, type_str)
 
 
 @pytest.mark.benchmark(group="GrammarNormalize")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_faster_normalize(benchmark: BenchmarkFixture, type_str):
-    benchmark(faster_eth_abi.grammar.normalize, type_str)
+    benchmark(batch, 1000, faster_eth_abi.grammar.normalize, type_str)
 
 
 @pytest.mark.benchmark(group="GrammarParse")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_parse(benchmark: BenchmarkFixture, type_str):
-    benchmark(eth_abi.grammar.parse, type_str)
+    benchmark(batch, 1000, eth_abi.grammar.parse, type_str)
 
 
 @pytest.mark.benchmark(group="GrammarParse")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_faster_parse(benchmark: BenchmarkFixture, type_str):
-    benchmark(faster_eth_abi.grammar.parse, type_str)
+    benchmark(batch, 1000, faster_eth_abi.grammar.parse, type_str)
--- benchmarks/test_registry_benchmarks.py
+++ benchmarks/test_registry_benchmarks.py
@@ -7,6 +7,9 @@ from pytest_codspeed import (
     BenchmarkFixture,
 )
 
+from benchmarks.batch import (
+    batch,
+)
 from benchmarks.type_strings import (
     type_strings,
 )
@@ -18,22 +21,22 @@ from faster_eth_abi.registry import (
 @pytest.mark.benchmark(group="RegistryGetEncoder")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_get_encoder(benchmark: BenchmarkFixture, type_str):
-    benchmark(registry.get_encoder, type_str)
+    benchmark(batch, 1000, registry.get_encoder, type_str)
 
 
 @pytest.mark.benchmark(group="RegistryGetEncoder")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_faster_get_encoder(benchmark: BenchmarkFixture, type_str):
-    benchmark(faster_registry.get_encoder, type_str)
+    benchmark(batch, 1000, faster_registry.get_encoder, type_str)
 
 
 @pytest.mark.benchmark(group="RegistryGetDecoder")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_get_decoder(benchmark: BenchmarkFixture, type_str):
-    benchmark(registry.get_decoder, type_str)
+    benchmark(batch, 1000, registry.get_decoder, type_str)
 
 
 @pytest.mark.benchmark(group="RegistryGetDecoder")
 @pytest.mark.parametrize("type_str", type_strings)
 def test_faster_get_decoder(benchmark: BenchmarkFixture, type_str):
-    benchmark(faster_registry.get_decoder, type_str)
+    benchmark(batch, 1000, faster_registry.get_decoder, type_str)
