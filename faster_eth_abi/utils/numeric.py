772151e7e6185ecf3d3a4e7afd9e7008e8e315a6
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-09-29 07:16:58 +0000

    Revert "feat: cache utils/numeric.py function results (#63)"
    This reverts commit aa64bd23bf7d50430b0847636b87754ecda617a5.

--- faster_eth_abi/utils/numeric.py
+++ faster_eth_abi/utils/numeric.py
@@ -1,7 +1,6 @@
 import decimal
 from typing import (
     Callable,
-    Dict,
     Final,
     Tuple,
 )
@@ -22,70 +21,41 @@ def ceil32(x: int) -> int:
     return x if remainder == 0 else x + 32 - (x % 32)
 
 
-_unsigned_integer_bounds_cache: Final[Dict[int, Tuple[int, int]]] = {}
-
-
 def compute_unsigned_integer_bounds(num_bits: int) -> Tuple[int, int]:
-    bounds = _unsigned_integer_bounds_cache.get(num_bits)
-    if bounds is None:
-        bounds = 0, 2**num_bits - 1
-        _unsigned_integer_bounds_cache[num_bits] = bounds
-    return bounds
-
-
-_signed_integer_bounds_cache: Final[Dict[int, Tuple[int, int]]] = {}
+    return 0, 2**num_bits - 1
 
 
 def compute_signed_integer_bounds(num_bits: int) -> Tuple[int, int]:
-    bounds = _signed_integer_bounds_cache.get(num_bits)
-    if bounds is None:
-        overflow_at = 2 ** (num_bits - 1)
-        min_value = -overflow_at
-        max_value = overflow_at - 1
-        bounds = min_value, max_value
-        _signed_integer_bounds_cache[num_bits] = bounds
-    return bounds
-
-
-_unsigned_fixed_bounds_cache: Final[Dict[Tuple[int, int], decimal.Decimal]] = {}
+    overflow_at = 2 ** (num_bits - 1)
+    min_value = -overflow_at
+    max_value = overflow_at - 1
+    return min_value, max_value
 
 
 def compute_unsigned_fixed_bounds(
     num_bits: int,
     frac_places: int,
 ) -> Tuple[decimal.Decimal, decimal.Decimal]:
-    upper = _unsigned_fixed_bounds_cache.get((num_bits, frac_places))
-    if upper is None:
-        int_upper = 2 ** (num_bits - 1) - 1
-    
-        with decimal_localcontext(abi_decimal_context):
-            upper = Decimal(int_upper) * TEN**-frac_places
+    int_upper = 2 ** (num_bits - 1) - 1
 
-        _unsigned_fixed_bounds_cache[(num_bits, frac_places)] = upper
-        
+    with decimal_localcontext(abi_decimal_context):
+        upper = Decimal(int_upper) * TEN**-frac_places
+
     return ZERO, upper
-
-
-_signed_fixed_bounds_cache: Final[Dict[Tuple[int, int], Tuple[decimal.Decimal, decimal.Decimal]]] = {}
 
 
 def compute_signed_fixed_bounds(
     num_bits: int,
     frac_places: int,
 ) -> Tuple[decimal.Decimal, decimal.Decimal]:
-    bounds = _signed_fixed_bounds_cache.get((num_bits, frac_places))
-    if bounds is None:
-        int_lower, int_upper = compute_signed_integer_bounds(num_bits)
-    
-        with decimal_localcontext(abi_decimal_context):
-            exp = TEN**-frac_places
-            lower = Decimal(int_lower) * exp
-            upper = Decimal(int_upper) * exp
+    int_lower, int_upper = compute_signed_integer_bounds(num_bits)
 
-        bounds = lower, upper
-        _signed_fixed_bounds_cache[(num_bits, frac_places)] = bounds
+    with decimal_localcontext(abi_decimal_context):
+        exp = TEN**-frac_places
+        lower = Decimal(int_lower) * exp
+        upper = Decimal(int_upper) * exp
 
-    return bounds
+    return lower, upper
 
 
 def scale_places(places: int) -> Callable[[decimal.Decimal], decimal.Decimal]:
