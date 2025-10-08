97bc00cc3e03cc6dc3f1a1100a7dbc4d06291978
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-08 01:07:39 -0400

    Update test_encode.py

--- tests/core/abi_tests/test_encode.py
+++ tests/core/abi_tests/test_encode.py
@@ -106,8 +106,7 @@ def test_abi_encode_raises_for_non_list_like_params(non_list_like_value):
         TypeError,
         # NOTE The `match` arg was commented out because faster-eth-abi might not raise
         # the same exception text, but it should still raise a TypeError like eth-abi
-        # match=f"The `types` value type must be one of list or tuple. "
-        f"Got {type(non_list_like_value)}",
+        # match=f"The `types` value type must be one of list or tuple. Got {type(non_list_like_value)}",
     ):
         encode(non_list_like_value, ["bytes"])
 
