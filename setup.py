7f7976255db6302460ca3860c492d31fb9e177da
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-17 16:44:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ else:
             "--install-types",
             "--disable-error-code=override",
             "--disable-error-code=unused-ignore",
+            "--disable-error-code=misc",
         ],
         group_name="faster_eth_abi",
         strict_dunder_typing=True,
