63f7cd7376bb06e7125e4a0278856ae2613cfb0f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-17 16:21:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ setup(
     install_requires=[
         "cchecksum>=0.2.6,<0.4",
         "faster-eth-utils>=2.0.0",
+        "eth-abi>=5.0.1,<6",
         "eth-typing>=3.0.0",
         "mypy_extensions",
         "parsimonious>=0.10.0,<0.11.0",
