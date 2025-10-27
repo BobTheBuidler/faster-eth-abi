75d55cd1bd085bb52542ac1a3867b42a3e4597e9
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-27 14:05:20 -0400

    Update registry.py

--- faster_eth_abi/registry.py
+++ faster_eth_abi/registry.py
@@ -585,12 +585,12 @@ class ABIRegistry(Copyable, BaseRegistry):
         cpy._encoder_cache = copy(self._encoder_cache)
         cpy._decoder_cache = {
             True: copy(self._decoder_cache[True]),
-            False: copy(self._decoder_cache[False),
+            False: copy(self._decoder_cache[False]),
         }
         cpy._tuple_encoder_cache = copy(self._tuple_encoder_cache)
         cpy._tuple_decoder_cache = {
             True: copy(self._tuple_decoder_cache[True]),
-            False: copy(self._tuple_decoder_cache[False),
+            False: copy(self._tuple_decoder_cache[False]),
         }
 
         return cpy
