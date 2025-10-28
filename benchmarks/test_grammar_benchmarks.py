ca9761276dfd88b5ebab2fc7e18f6bde2b269368
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-28 00:58:26 -0400

    feat(benchmark): benchmark the rest of registry submodule (#167)

--- benchmarks/test_grammar_benchmarks.py
+++ benchmarks/test_grammar_benchmarks.py
@@ -9,30 +9,30 @@ from benchmarks.batch import (
     batch,
 )
 from benchmarks.type_strings import (
-    type_strings,
+    TYPE_STRINGS,
 )
 import faster_eth_abi.grammar
 
 
 @pytest.mark.benchmark(group="GrammarNormalize")
-@pytest.mark.parametrize("type_str", type_strings)
+@pytest.mark.parametrize("type_str", TYPE_STRINGS)
 def test_normalize(benchmark: BenchmarkFixture, type_str):
     benchmark(batch, 5000, eth_abi.grammar.normalize, type_str)
 
 
 @pytest.mark.benchmark(group="GrammarNormalize")
-@pytest.mark.parametrize("type_str", type_strings)
+@pytest.mark.parametrize("type_str", TYPE_STRINGS)
 def test_faster_normalize(benchmark: BenchmarkFixture, type_str):
     benchmark(batch, 5000, faster_eth_abi.grammar.normalize, type_str)
 
 
 @pytest.mark.benchmark(group="GrammarParse")
-@pytest.mark.parametrize("type_str", type_strings)
+@pytest.mark.parametrize("type_str", TYPE_STRINGS)
 def test_parse(benchmark: BenchmarkFixture, type_str):
     benchmark(batch, 5000, eth_abi.grammar.parse, type_str)
 
 
 @pytest.mark.benchmark(group="GrammarParse")
-@pytest.mark.parametrize("type_str", type_strings)
+@pytest.mark.parametrize("type_str", TYPE_STRINGS)
 def test_faster_parse(benchmark: BenchmarkFixture, type_str):
     benchmark(batch, 5000, faster_eth_abi.grammar.parse, type_str)
