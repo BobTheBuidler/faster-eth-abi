590bf0d399fa9e15a1fcc0f923701d704a45f204
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-30 01:52:49 -0400

    chore: pin `eth-abi` (#193)

--- setup.py
+++ setup.py
@@ -117,7 +117,7 @@ setup(
     install_requires=[
         "cchecksum>=0.2.6,<0.4",
         "faster-eth-utils==5.3.16",
-        "eth-abi>=5.0.1,<6",
+        "eth-abi==5.2.0",
         "eth-typing==5.2.1",
         "mypy_extensions",
         "parsimonious>=0.10.0,<0.11.0",
