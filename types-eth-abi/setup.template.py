cabe612a27d52620c4412a82b55cad9b6da373e9
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-10-30 01:03:48 +0000

    fix

--- types-eth-abi/setup.template.py
+++ types-eth-abi/setup.template.py
@@ -20,6 +20,8 @@ setup(
     python_requires="{{PYTHON_REQUIRES}}",
     zip_safe=False,
     include_package_data=True,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     classifiers=[
         "Typing :: Stubs Only",
         "Programming Language :: Python :: 3",
