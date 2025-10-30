55309e8a6760fed49048929bfc35506f6f392e59
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-10-30 00:40:09 +0000

    fix stuf action

---
+++ types-eth-abi/setup.template.py
@@ -0,0 +1,27 @@
+from setuptools import (
+    setup,
+)
+
+setup(
+    name="types-eth-utils",
+    version="{{VERSION}}",
+    description="Type stubs for eth-abi and faster-eth-abi",
+    author="BobTheBuidler",
+    author_email="bobthebuidlerdefi@gmail.com",
+    url="{{URL}}",
+    license="{{LICENSE}}",
+    keywords="{{KEYWORDS}}",
+    packages=["faster_eth_abi", "eth_abi"],
+    package_data={
+        "faster_eth_abi": ["*.pyi", "py.typed"],
+        "eth_abi": ["*.pyi", "py.typed"],
+    },
+    install_requires=[],
+    python_requires="{{PYTHON_REQUIRES}}",
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Typing :: Stubs Only",
+        "Programming Language :: Python :: 3",
+    ],
+)
