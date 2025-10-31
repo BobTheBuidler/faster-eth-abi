fb521429d3359db56e460521d7fa542b0cb08478
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-10-30 21:10:01 -0400

    feat(mypy): include docstrings in stub package (#203)
    * feat(mypy): include docstrings in stub package
    
    * Update stubgen.yaml
    
    * chore(mypy): update type stubs [automated] (#205)
    
    Co-authored-by: BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- types-eth-abi/faster_eth_abi/from_type_str.pyi
+++ types-eth-abi/faster_eth_abi/from_type_str.pyi
@@ -8,5 +8,16 @@ TType = TypeVar('TType', bound=type['BaseCoder'])
 OldFromTypeStr: Incomplete
 NewFromTypeStr: Incomplete
 
-def parse_type_str(expected_base: str | None = None, with_arrlist: bool = False) -> Callable[[OldFromTypeStr[TType]], 'NewFromTypeStr[TType]']: ...
-def parse_tuple_type_str(old_from_type_str: OldFromTypeStr[TType]) -> NewFromTypeStr[TType]: ...
+def parse_type_str(expected_base: str | None = None, with_arrlist: bool = False) -> Callable[[OldFromTypeStr[TType]], 'NewFromTypeStr[TType]']:
+    """
+    Used by BaseCoder subclasses as a convenience for implementing the
+    ``from_type_str`` method required by ``ABIRegistry``.  Useful if normalizing
+    then parsing a type string with an (optional) expected base is required in
+    that method.
+    """
+def parse_tuple_type_str(old_from_type_str: OldFromTypeStr[TType]) -> NewFromTypeStr[TType]:
+    """
+    Used by BaseCoder subclasses as a convenience for implementing the
+    ``from_type_str`` method required by ``ABIRegistry``.  Useful if normalizing
+    then parsing a tuple type string is required in that method.
+    """
