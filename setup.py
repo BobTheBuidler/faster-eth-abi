2e8e4043106af15adcd7b664a39259294f8d80ba
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-11-01 18:20:31 -0400

    fix(test): fix tox 3.8

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ extras_require = {
         "sphinx>=6.0.0",
         "sphinx-autobuild>=2021.3.14",
         "sphinx_rtd_theme>=1.0.0",
-        "towncrier>=25,<26",
+        "towncrier>=24,<26",
     ],
     "test": [
         "pytest>=7.0.0",
