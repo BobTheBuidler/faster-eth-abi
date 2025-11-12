78899587bb7dd1c464ef7eb28911bef7ee02c51e
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-11-12 18:19:53 -0400

    chore: bump version to 5.2.21

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ else:
 setup(
     name="faster_eth_abi",
     # *IMPORTANT*: Don't manually change the version here. See Contributing docs for the release process.
-    version="5.2.20",
+    version="5.2.21",
     description="""A ~2-6x faster fork of eth_abi: Python utilities for working with Ethereum ABI definitions, especially encoding and decoding. Implemented in C.""",
     long_description=long_description,
     long_description_content_type="text/markdown",
