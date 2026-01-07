05b2fbe3820cf153103858e4415c4583a64007f2
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-07 16:35:13 -0400

    Update __init__.py

--- faster_eth_abi/__init__.py
+++ faster_eth_abi/__init__.py
@@ -1,5 +1,4 @@
 from importlib.metadata import (
-    PackageNotFoundError,
     version as __version,
 )
 
@@ -10,7 +9,4 @@ from faster_eth_abi.abi import (
     is_encodable_type,
 )
 
-try:
-    __version__ = __version("faster-eth-abi")
-except PackageNotFoundError:  # pragma: no cover - fallback for source checkouts
-    __version__ = "0+unknown"
+__version__ = __version("faster-eth-abi")
