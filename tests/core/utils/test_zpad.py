0011676338f3527c3dff3c54c9473c8449e937fd
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2026-07-29 02:00:34 -0400

    perf: replace curried padding helpers with direct functions

--- tests/core/utils/test_zpad.py
+++ tests/core/utils/test_zpad.py
@@ -1,7 +1,12 @@
 import pytest
 
 from eth_abi.utils.padding import (
+    fpad,
+    fpad32,
     zpad,
+    zpad32,
+    zpad32_right,
+    zpad_right,
 )
 
 
@@ -16,4 +21,29 @@ from eth_abi.utils.padding import (
 )
 def test_zpadding(value, length, expected):
     actual = zpad(value, length)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "func,value,length,expected",
+    (
+        (zpad_right, b"abc", 5, b"abc\x00\x00"),
+        (fpad, b"abc", 5, b"\xff\xffabc"),
+    ),
+)
+def test_padding_helpers(func, value, length, expected):
+    actual = func(value, length)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "func,value,expected",
+    (
+        (zpad32, b"abc", (b"\x00" * 29) + b"abc"),
+        (zpad32_right, b"abc", b"abc" + (b"\x00" * 29)),
+        (fpad32, b"abc", (b"\xff" * 29) + b"abc"),
+    ),
+)
+def test_32_byte_padding_helpers(func, value, expected):
+    actual = func(value)
     assert actual == expected
